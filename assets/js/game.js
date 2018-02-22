bb716ce0c9e06724294dbe2d1a2da5928105b094
dimstar <kylermagee@gmail.com>
2018-02-21 21:30:36 -0800

    Scafolded the site's main game object added other skeleton crew of files for assets, etc.

---
+++ assets/js/game.js
@@ -0,0 +1,22 @@
+/*
+notes: use arrow functions! (if possible)
+
+The game object and scafolded files for the Gang of Four Fighting Game (based on Star Wars RPG)
+
+Game: "this is our main game object, contains and scopes everything",
+    fightpit: "this contains who is attacker, defender",
+        attacker: ,
+        defender: ,
+        terrain: "gives modifiers, size effects range, etc.",
+    player: "this is the, you guessed, the player. the player is playing as a mech pilot",
+    mechs:  "these are mechs with all different properties like Health, Attack Power, Counter Attack Power",
+        slugtosser
+        gobstomper
+        atomsmasher
+        fornicator
+    actions: "not sure what to call this but it 'does things'"
+        attack:
+        defend: "maybe I don't need this?"
+        takeDamage:
+
+*/
